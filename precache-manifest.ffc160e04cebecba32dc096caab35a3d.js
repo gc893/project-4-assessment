5541a26206d1a5cce6d5376745be54b4ff986817
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-09-22 14:05:08 -0400

    Updates

---
+++ precache-manifest.ffc160e04cebecba32dc096caab35a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bebc408927c70183493dcc352e47493",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "960463b743faed4f4852",
+    "url": "/project-4-assessment/static/css/main.ef2e0a69.chunk.css"
+  },
+  {
+    "revision": "5d87b93650848dad4642",
+    "url": "/project-4-assessment/static/js/2.8711a773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.8711a773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960463b743faed4f4852",
+    "url": "/project-4-assessment/static/js/main.af7a2ce5.chunk.js"
+  },
+  {
+    "revision": "cb84208017e68e418b94",
+    "url": "/project-4-assessment/static/js/runtime-main.932b97e7.js"
+  }
+]);
